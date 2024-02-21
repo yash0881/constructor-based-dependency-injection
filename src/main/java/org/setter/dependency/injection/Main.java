61441f6771b77d91c04b50d9d2dba58e70530a79
Yash Gupta <yash.itca24@gmail.com>
2024-02-22 02:18:50 +0530

    22-02-2024

---
+++ src/main/java/org/setter/dependency/injection/Main.java
@@ -0,0 +1,15 @@
+package org.setter.dependency.injection;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        String location = "/applicationContext.xml";
+        ApplicationContext context = new ClassPathXmlApplicationContext(location);
+        Student s = (Student) context.getBean("stdId");
+        System.out.println(s);
+    }
+}
